43afc6fbcb5114eb2b0d68d99195c99a073d6ad5
Georg Steinbacher <georgsteinbacher@gmx.net>
2020-08-10 17:24:35 +0200

    init

---
+++ instant/src/main/java/com/farmdok/multi/instant/InstantActivity.kt
@@ -0,0 +1,14 @@
+package com.farmdok.multi.instant
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
+private const val TAG = "InstantActivity"
+
+class InstantActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_instant)
+    }
+}
